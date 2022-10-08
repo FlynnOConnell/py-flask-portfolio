52a68542e74c8a6a5171a9290d526791e906752d
Flynn P O'Connell <oconnell@binghamton.edu>
2022-10-08 01:40:58 -0400

    add resume as png

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -6,14 +6,15 @@ Entry point for dataviewer app. Flask docs recommend larger app initializing to 
 
 
 import os
+from pathlib import Path
 from flask import (Flask, current_app)
 from werkzeug.local import LocalProxy
-
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"C:\Users\flynn\repos\web_dataviewer\JSON_credentials.json"
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 dirname = os.path.dirname(os.path.abspath(__file__))
 logger = LocalProxy(lambda: current_app.logger)
+
 
 
 def create_app(test_config=None):
@@ -22,17 +23,13 @@ def create_app(test_config=None):
     # commited to vc
     app = Flask(__name__, instance_relative_config=True)
 
-    # set some default configs
-    app.config.from_mapping(
-            SECRET_KEY='dev',
-            UPLOAD_FOLDER=dirname + '/flaskr/static/uploads',
-            DATABASE=os.path.join(app.instance_path, 'web_dataviewer.sqlite'),
-            MAX_CONTENT_LENGTH=16 * 1024 * 1024
-        )
+    # app.config.from_object('yourapplication.default_settings')
+    # app.config.from_envvar('YOURAPPLICATION_SETTINGS')
+    import os
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_pyfile('default_settings.py', silent=True)
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
@@ -46,3 +43,9 @@ def create_app(test_config=None):
     from flaskr import home
     app.register_blueprint(home.bp)
     return app
+
+if __name__ == "__main__":
+    from waitress import serve
+    app = create_app()
+    app.config["DEBUG"]=True
+    serve(app, host='0.0.0.0', port=8080)
