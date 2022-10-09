25d3dda0c24fcd481157d71fc2b9d802d5bd7288
Flynn P O'Connell <oconnell@binghamton.edu>
2022-10-08 22:59:35 -0400

    logo, styling, add badges to py analysis

--- flaskr/static/homepage/js/custom.js
+++ flaskr/static/homepage/js/custom.js
@@ -1,8 +1,27 @@
-function myFunction() {
-          var x = document.getElementById("myTopnav");
-          if (x.className === "topnav") {
-            x.className += " responsive";
-          } else {
-            x.className = "topnav";
-      }
+
+
+
+
+
+function mainFunction() {
+    let active = $(this)
+    let img
+    if(active.attr("id") === "button-dev"){
+        img = $("#resume")
+        $("#resume2").addClass("hide-this")
+        img.removeClass("hide-this")
     }
+    else {
+        img = $("#resume2")
+        $("#resume").addClass("hide-this")
+        img.removeClass("hide-this")
+     }
+}
+
+$(document).ready(function(){
+    $('[name="resume-button-name"]').on('click', mainFunction)
+})
+
+
+
+
